85c02f35239728707d2dbbe1c60bf7d7a2a0e13c
Rik-F5 <star.astroboy@gmail.com>
2024-04-27 02:07:07 +1200

    Finished the download functions.

--- README.md
+++ README.md
@@ -8,9 +8,16 @@ source .venv/bin/activate
 
 python -m pip install -r requirements.txt
 
-python solid.py --media <Media folder>
+python dev.py --media <Media folder> (Only sync the files)
 
+python dev.py --db   (Only scrap the website and save to DB)
 
+python dev.py --db true --media <Media folder> (Sync files and save to DB)
 
+python dev.py --count <int> concurrent HTTP requests. Default is 100, CF will enforce the concurrent for a single source IP with 502/520. Reduce it if you encounter download failures.
+
+Linux kernel tweak:
 ulimit -n 30000
+
+python 3.12+ is needed to avoid an asyncio performance bug
 
--- dev.py
+++ dev.py
@@ -22,11 +22,10 @@ logging.basicConfig(
 logger = logging.getLogger("areq")
 logging.getLogger("chardet.charsetprober").disabled = True
 
-async def fetch_html(url: str, session: ClientSession, **kwargs) -> str:
+async def fetch_html(url, session, **kwargs) -> str:
     semaphore = kwargs['semaphore']
-    headers = {'connection': 'keep-alive'}
     async with semaphore:
-        resp = await session.request(method="GET", url=url, headers=headers)
+        resp = await session.request(method="GET", url=url)
         logger.debug("Request Headers for [%s]: [%s]", unquote(url), resp.request_info.headers)
         resp.raise_for_status()       
         logger.debug("Response Headers for [%s]: [%s]", unquote(url), resp.headers)
@@ -34,7 +33,7 @@ async def fetch_html(url: str, session: ClientSession, **kwargs) -> str:
         html = await resp.text()
         return html
 
-async def parse(url: str, session: ClientSession, **kwargs) -> set:
+async def parse(url, session, **kwargs) -> set:
     files = []
     directories = []
     try:
@@ -75,70 +74,95 @@ async def parse(url: str, session: ClientSession, **kwargs) -> set:
                 directories.append(urljoin(url, href))
         return files, directories
 
+async def need_download(file, **kwargs):
+    url, filename, timestamp, filesize = file
+    file_path = os.path.join(kwargs['media'], filename.lstrip('/'))
+    if not os.path.exists(file_path):
+        return True 
+    else:
+        current_timestamp = os.path.getmtime(file_path)
+        if timestamp > current_timestamp:
+            current_filesize = os.path.getsize(file_path)
+            if filesize == current_filesize:
+                return False
+    return True
 
-async def write_one(database, url: str, db, **kwargs) -> list:
-    files, directories = await parse(url=url, **kwargs)
+async def download(file, session, **kwargs):
+    url, filename, timestamp, filesize = file
+    semaphore = kwargs['semaphore']
+    async with semaphore:
+        response = await session.get(url)
+        if response.status == 200:
+            file_path = os.path.join(kwargs['media'], filename.lstrip('/'))
+            os.umask(0)
+            os.makedirs(os.path.dirname(file_path), mode=0o777, exist_ok=True)
+            async with aiofiles.open(file_path, 'wb') as f:
+                logger.debug("Starting to write file: %s", filename)
+                await f.write(await response.content.read())
+                logger.debug("Finish to write file: %s", filename)
+            os.chmod(file_path, 0o777)
+            logger.info("Downloaded: %s", filename)
+        else:
+            logger.info("Failed to download: %s [Response code: %s]", filename, response.status)
+
+
+async def download_files(files, session, **kwargs):
+    download_tasks = []
+    for file in files:
+        if await need_download(file, **kwargs):
+            task = asyncio.create_task(download(file, session, **kwargs))
+            download_tasks.append(task)
+    await asyncio.gather(*download_tasks)
+
+    
+
+
+async def write_one(url, session, db_session, **kwargs) -> list:
+    files, directories = await parse(url=url, session=session, **kwargs)
     if not files:
         return directories
-    await db.executemany('INSERT OR REPLACE INTO files VALUES (?, ?, ?, ?)', files)
-    await db.commit()
-    logger.debug("Wrote results for source URL: %s", unquote(url))
+    if db_session:
+        await db_session.executemany('INSERT OR REPLACE INTO files VALUES (?, ?, ?, ?)', files)
+        await db_session.commit()
+        logger.debug("Wrote results for source URL: %s", unquote(url))
+    if kwargs['media']:
+        await download_files(files=files, session=session, **kwargs)
     return directories
 
 
-async def bulk_crawl_and_write(database, url: str, session, db, **kwargs) -> None:
-    if session.closed or not session:
-        session = await ClientSession(connector=TCPConnector(ssl=False, limit=0, ttl_dns_cache=600))
-    if not db:
-        db = await aiosqlite.connect(database)
+async def bulk_crawl_and_write(url, session, db_session, **kwargs) -> None:
     tasks = []
-    directories = await write_one(database=database, url=url, session=session, db=db, **kwargs)
+    directories = await write_one(url=url, session=session, db_session=db_session, **kwargs)
     for url in directories:
-        task = asyncio.create_task(bulk_crawl_and_write(database=database, url=url, session=session, db=db, **kwargs))
+        task = asyncio.create_task(bulk_crawl_and_write(url=url, session=session, db_session=db_session, **kwargs))
         tasks.append(task)
     await asyncio.gather(*tasks)
 
 
-async def process_temp_database(temp_db_file, db_file, media_path, args):
-    try:
-        if not args.no_download:
-            async with aiohttp.ClientSession(connector=aiohttp.TCPConnector(ssl=False)) as session:
-                temp_conn = sqlite3.connect(temp_db_file)
-                temp_c = temp_conn.cursor()
-                temp_c.execute('SELECT COUNT(*) FROM files')
-                total_records = temp_c.fetchone()[0]
-                chunk_size = 1000  # Adjust the chunk size as needed
-                offset = 0
-                while offset < total_records:
-                    temp_c.execute('SELECT * FROM files LIMIT ? OFFSET ?', (chunk_size, offset))
-                    temp_files = temp_c.fetchall()
-                    await store_in_database(db_file, temp_files, media_path, session)
-                    offset += chunk_size
-        else:
-            print("Skipping download.")
-    finally:
-        temp_conn.close()
-
-
-
 async def main() :
     parser = argparse.ArgumentParser()
-    parser.add_argument("--media", type=str, default=os.path.join(os.path.dirname(__file__), "media"), help="Path to store downloaded media files")
+    parser.add_argument("--media", type=str, default=None, help="Path to store downloaded media files")
     parser.add_argument("--count", type=int, default=100, help="Max concurrent HTTP Requests")
     parser.add_argument("--debug", default=False, help="Verbose debug")
+    parser.add_argument("--db", default=False, help="Create db")
     parser.add_argument("--url", type=str, default="https://emby.xiaoya.pro/", help="Verbose debug")
+    
     args = parser.parse_args()
     if args.debug:
         logging.getLogger("areq").setLevel(logging.DEBUG)
     url = args.url
     database = "file.db"
     semaphore = asyncio.Semaphore(args.count)
-    async with aiosqlite.connect(database) as db:
-        await db.execute('''CREATE TABLE IF NOT EXISTS files
+    db_session = None
+    if args.db:
+        db_session = await aiosqlite.connect(database)
+        await db_session.execute('''CREATE TABLE IF NOT EXISTS files
                          (url TEXT PRIMARY KEY, filename TEXT, timestamp INTEGER, filesize INTERGER)''')
-        async with ClientSession(connector=TCPConnector(ssl=False, limit=0, ttl_dns_cache=600)) as session:
-            await bulk_crawl_and_write(database=database, url=url, session=session, db=db, semaphore=semaphore)
+    async with ClientSession(connector=TCPConnector(ssl=False, limit=0, ttl_dns_cache=600)) as session:
+        await bulk_crawl_and_write(url=url, session=session, db_session=db_session, semaphore=semaphore, media=args.media)
+    
+    
 
 if __name__ == "__main__":
-    assert sys.version_info >= (3, 10), "Script requires Python 3.10+."
+    assert sys.version_info >= (3, 12), "Script requires Python 3.12+."
     asyncio.run(main())
