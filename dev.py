26d47217612e1741476cc84e67a34efc79e00f15
Rik-F5 <star.astroboy@gmail.com>
2024-04-26 13:13:55 +1200

    Added semaphore for concurrency control

--- dev.py
+++ dev.py
@@ -1,7 +1,5 @@
 
-import asyncio
 import logging
-import re
 import sys
 from typing import IO
 import urllib.error
@@ -10,6 +8,7 @@ from urllib.parse import urljoin, urlparse, unquote
 from bs4 import BeautifulSoup
 from datetime import datetime
 
+import asyncio
 import aiofiles
 import aiohttp
 from aiohttp import ClientSession, TCPConnector
@@ -24,22 +23,15 @@ logging.basicConfig(
 logger = logging.getLogger("areq")
 logging.getLogger("chardet.charsetprober").disabled = True
 
-HREF_RE = re.compile(r'href="(.*?)"')
-
-async def fetch_html(url: str, session: ClientSession, **kwargs) -> str:
-    """GET request wrapper to fetch page HTML.
-
-    kwargs are passed to `session.request()`.
-    """
-
-    resp = await session.request(method="GET", url=url, **kwargs)
-    #resp.raise_for_status()
-    #logger.info("Got response [%s] for URL: %s", resp.status, unquote(url))
-    html = await resp.text()
-    return html
+async def fetch_html(url: str, session: ClientSession, semaphore, **kwargs) -> str:
+    async with semaphore:
+        resp = await session.request(method="GET", url=url, **kwargs)
+        resp.raise_for_status()
+        logger.debug("Got response [%s] for URL: %s", resp.status, unquote(url))
+        html = await resp.text()
+        return html
 
 async def parse(url: str, session: ClientSession, **kwargs) -> set:
-    """Find HREFs in the HTML of `url`."""
     files = []
     directories = []
     try:
@@ -62,15 +54,11 @@ async def parse(url: str, session: ClientSession, **kwargs) -> set:
         return files, directories
     else:
         soup = BeautifulSoup(html, 'html.parser')
-        logger.debug("URL: %s", url)
         for link in soup.find_all('a'):
-            logger.debug("Find link: %s", link)
             href = link.get('href')
-            logger.debug("Find href: %s", href)
             if href != '../' and not href.endswith('/'):
                 try:
                     abslink = urljoin(url, href)
-                    logger.debug("Find URL: %s", abslink)
                 except (urllib.error.URLError, ValueError):
                     logger.exception("Error parsing URL: %s", unquote(link))
                 pass
@@ -80,45 +68,47 @@ async def parse(url: str, session: ClientSession, **kwargs) -> set:
                 timestamp_unix = int(timestamp.timestamp())
                 filesize = link.next_sibling.strip().split()[2]
                 files.append((abslink, filename, timestamp_unix, filesize))
-                logger.debug("Found %d files for %s", len(files), unquote(url))
             elif href != '../':
                 directories.append(urljoin(url, href))
         return files, directories
 
 
-async def write_one(database: IO, url: str, **kwargs) -> list:
-    """Write the found HREFs from `url` to `file`."""
+async def write_one(database: IO, url: str, db, **kwargs) -> list:
     files, directories = await parse(url=url, **kwargs)
     if not files:
         return directories
-    logger.debug(files)
-    #async with aiosqlite.connect(database) as db:
-    #    await db.execute('''CREATE TABLE IF NOT EXISTS files
-    #                     (url TEXT PRIMARY KEY, filename TEXT, timestamp INTEGER, filesize INTERGER)''')
-    #    await db.executemany('INSERT OR REPLACE INTO files VALUES (?, ?, ?, ?)', files)
-    #    await db.commit()
-    #    logger.info("Wrote results for source URL: %s", unquote(url))
+    await db.execute('''CREATE TABLE IF NOT EXISTS files
+                         (url TEXT PRIMARY KEY, filename TEXT, timestamp INTEGER, filesize INTERGER)''')
+    await db.executemany('INSERT OR REPLACE INTO files VALUES (?, ?, ?, ?)', files)
+    await db.commit()
+    logger.debug("Wrote results for source URL: %s", unquote(url))
     return directories
 
-async def bulk_crawl_and_write(database: IO, url: str, **kwargs) -> None:
-    """Crawl & write concurrently to `file` for multiple `urls`."""
-    async with ClientSession(connector=TCPConnector(ssl=False, limit=10, ttl_dns_cache=600)) as session:
-        tasks = []
-        directories = await write_one(database=database, url=url, session=session, **kwargs)
-        for url in directories:
-            task = asyncio.create_task(bulk_crawl_and_write(database=database, url=url, **kwargs))
-            tasks.append(task)
-        await asyncio.gather(*tasks)
+async def bulk_crawl_and_write(database: IO, url: str, session, db, **kargs) -> None:
+    if not session:
+        session = ClientSession(connector=TCPConnector(ssl=False, limit=10, ttl_dns_cache=600))
+    if not db:
+        db = await aiosqlite.connect(database)
+    tasks = []
+    directories = await write_one(database=database, url=url, session=session, db=db, **kargs)
+    for url in directories:
+        task = asyncio.create_task(bulk_crawl_and_write(database=database, url=url, session=session, db=db, **kargs))
+        tasks.append(task)
+    await asyncio.gather(*tasks)
 
-if __name__ == "__main__":
+
+async def main() :
     import pathlib
     import sys
-
     assert sys.version_info >= (3, 7), "Script requires Python 3.7+."
     here = pathlib.Path(__file__).parent
-
     url = "https://emby.xiaoya.pro/"
-
     database = "file.db"
+    db = await aiosqlite.connect(database)
+    semaphore = asyncio.Semaphore(100)
+    async with ClientSession(connector=TCPConnector(ssl=False, limit=0, ttl_dns_cache=600)) as session:
+        await bulk_crawl_and_write(database=database, url=url, session=session, db=db, semaphore=semaphore)
+    await db.close()
 
-    asyncio.run(bulk_crawl_and_write(database=database, url=url))
+if __name__ == "__main__":
+    asyncio.run(main())
